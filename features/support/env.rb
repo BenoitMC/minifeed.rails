b0c7e67311643e6579e83dbb19ac105ea2d83d9c
Benoit MARTIN-CHAVE <benoitmartinchave@gmail.com>
2015-07-10 09:58:21 +0200

    Add FactoryGirl DSL to cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -56,3 +56,4 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+World(FactoryGirl::Syntax::Methods)
