18ca789fd627ee1fa2d804f04c6423af3586b939
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2015-12-09 14:09:51 +0100

    Fix ExceptionNotification config

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,7 +2,7 @@ if %w(production staging).include?(Rails.env)
   Rails.application.config.middleware.use(ExceptionNotification::Rack,
     :email => {
       :email_prefix         => "[Mom #{Rails.env}] ",
-      :sender_address       => %{"BuyCo Notifier" <mom@example.org>},
+      :sender_address       => %{"Mom Notifier" <mom@example.org>},
       :exception_recipients => %w{support@agilidee.com},
     }
   )
