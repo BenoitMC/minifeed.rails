ccc28d049180b37db356ca3bc1cae916d2254905
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2016-06-03 09:24:18 +0200

    Change poltergeist/phantomjs version

--- features/support/common.rb
+++ features/support/common.rb
@@ -2,16 +2,25 @@ require "capybara/poltergeist"
 
 ActionController::Base.allow_rescue = false
 
+phantomjs_version = "2.1.1"
+phantomjs_binary = `which phantomjs-#{phantomjs_version} phantomjs`.split("\n").first
+raise "invalid phantomjs version" unless `#{phantomjs_binary} -v`.strip == phantomjs_version
+# You can download phantomjs here : https://bitbucket.org/ariya/phantomjs/downloads/
+# Semaphore setup commmand : change-phantomjs-version 2.1.1
+
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app,
     :debug       => false,
     :window_size => [1680, 1050],
     :timeout     => 60,
+    :phantomjs   => phantomjs_binary,
   )
 end
 
-Capybara.current_driver    = :poltergeist
-Capybara.javascript_driver = :poltergeist
+Capybara.default_driver        = :poltergeist
+Capybara.javascript_driver     = :poltergeist
+Capybara.current_driver        = :poltergeist
+Capybara.default_max_wait_time = 3
 
 DatabaseCleaner.strategy                      = :transaction
 Cucumber::Rails::Database.javascript_strategy = :transaction
