ce9c424112eda9c7b4b2c8a612fb284119dbb6cb
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2016-06-13 16:42:09 +0200

    Enable desktop delivery on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.delivery_method = :desktop_delivery
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 
   # Raises error for missing translations
