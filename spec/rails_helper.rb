754c461cb3af93dade25d5177cad23eb9c17e205
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2016-07-08 11:00:59 +0200

    Add rails-controller-testing + update gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,5 +67,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
