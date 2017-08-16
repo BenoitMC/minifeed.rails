45fdbd4023a6cd8b8794d2bd7a0602410acfaea3
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2017-08-16 16:43:40 +0200

    Add DJ queue adapter

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,4 +1,6 @@
 if defined? Delayed::Job
+  Rails.application.config.active_job.queue_adapter = :delayed_job
+
   if Rails.env.test? || Rails.env.development?
     Delayed::Worker.delay_jobs = false
   else
