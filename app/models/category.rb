0cf1f64fb4512192d70aa06d3fecf37664829177
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2018-04-28 17:25:09 +0200

    Category has many feeds

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many   :feeds, dependent: :nullify
 
   validates :user, presence: true
   validates :name, presence: true
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe Category, type: :model do
   it { is_expected.to belong_to :user }
+  it { is_expected.to have_many(:feeds).dependent(:nullify) }
 
   it { is_expected.to validate_presence_of :user }
   it { is_expected.to validate_presence_of :name }
