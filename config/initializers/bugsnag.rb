f4c707dea2c43ef0e291299a492bf76beb6afee0
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2018-04-29 16:22:49 +0200

    Bugsnag config

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
 Bugsnag.configure do |config|
-  config.api_key = ""
-  config.notify_release_stages = ["production", "staging"]
+  config.api_key = ENV["BUGSNAG_API_KEY"]
+  config.notify_release_stages = %w(production staging)
 end
