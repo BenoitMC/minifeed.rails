299bf5c6e61ed477418126b08e83e355c5440117
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2018-05-04 23:21:41 +0200

    Update cucumber config

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,18 +1,4 @@
 require "cucumber/rails"
 require "agilibox/cucumber_config"
 Agilibox::CucumberConfig.require_all_helpers!
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    :chromeOptions => {args: %w(headless disable-gpu window-size=1680,1050)},
-  )
-
-  Capybara::Selenium::Driver.new(app,
-    :browser              => :chrome,
-    :desired_capabilities => capabilities,
-  )
-end
-
-Capybara.default_driver    = :headless_chrome
-Capybara.current_driver    = :headless_chrome
-Capybara.javascript_driver = :headless_chrome
+Agilibox::CucumberConfig.require_chrome_headless!
