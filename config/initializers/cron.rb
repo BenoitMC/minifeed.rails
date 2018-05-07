fdadf011d60f5aa832fa7e34b848bc04749fb9f6
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2018-05-07 18:37:12 +0200

    Scheduler : enable on server only

--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -1,5 +1,7 @@
 scheduler = Rufus::Scheduler.singleton
 
-scheduler.every "5m", first: :now, overlap: false do
-  Feed::ImportAllService.call
+if defined?(Rails::Server)
+  scheduler.every "5m", first: :now, overlap: false do
+    Feed::ImportAllService.call
+  end
 end
