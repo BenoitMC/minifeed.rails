c37983f7c3979d9d23740a1c6b562946544f1719
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2018-05-09 19:50:38 +0200

    Import feeds concurrently

--- app/services/feed/import_all_service.rb
+++ app/services/feed/import_all_service.rb
@@ -1,15 +1,24 @@
 class Feed::ImportAllService < Service
+  POOL_SIZE = 10
+
   def call
     feeds.each do |feed|
-      Feed::ImportService.call(feed)
+      pool.post do
+        Feed::ImportService.call(feed)
+      end
     end
+
+    pool.shutdown
+    pool.wait_for_termination
   end
 
   private
 
   def feeds
-    Feed
-      .preload(:user, :category)
-      .reorder(:last_update_at)
+    Feed.preload(:user, :category)
+  end
+
+  def pool
+    @pool ||= Concurrent::FixedThreadPool.new(POOL_SIZE)
   end
 end
