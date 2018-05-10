562dbce862f2d1a4ec51c85613505983293961ac
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2018-05-10 18:47:15 +0200

    Change cron

--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -1,7 +1,7 @@
 scheduler = Rufus::Scheduler.singleton
 
 if ENV["MINIFEED_AUTOIMPORT"].to_s == "true"
-  scheduler.every "5m", first: :now, overlap: false do
+  scheduler.every "5m", first_in: "30s", overlap: false do
     Feed::ImportAllService.call
   end
 end
