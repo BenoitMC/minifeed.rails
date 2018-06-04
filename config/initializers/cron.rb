4d3bcc842702640248e573adc5080a8bc771506d
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2018-06-04 17:54:02 +0200

    Bugsnag + rufus config

--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -1,5 +1,9 @@
 scheduler = Rufus::Scheduler.singleton
 
+def scheduler.on_error(_job, error)
+  Bugsnag.notify(error)
+end
+
 if ENV["MINIFEED_AUTOIMPORT"].to_s == "true"
   scheduler.every "5m", first_in: "30s", overlap: false do
     Feed::ImportAllService.call
