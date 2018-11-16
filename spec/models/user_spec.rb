a51f8ecc88c7e135b33e5f60ab1f89139983875c
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2018-11-16 17:23:13 +0100

    Remove useless code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,22 +13,4 @@ describe User do
     user = create(:user)
     expect(user.auth_token).to be_present
   end
-
-  describe "#id" do
-    it "should be present on build" do
-      expect(User.new.id).to be_present
-    end
-
-    it "should not change on saves" do
-      user = build(:user)
-      id   = user.id
-      expect(id).to be_present
-
-      user.update!(email: "user2@example.org")
-      expect(user.id).to eq id
-
-      user.update!(email: "user3@example.org")
-      expect(user.id).to eq id
-    end
-  end # describe "#id"
 end
