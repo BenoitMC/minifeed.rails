53388b0a6d4f600793fbf8fabbb1df0adb4d2229
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2019-01-18 17:23:54 +0100

    Add Procfile + seeds

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: MINIFEED_AUTOIMPORT=true bundle exec rails s -p $PORT -e 'production'
+release: bundle exec rake db:migrate db:seed
---
+++ spec/seeds/seeds_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "seeds" do
+  it "should create sample data" do
+    load Rails.root.join("db", "seeds.rb")
+    expect(User.count).to eq 1
+    expect(Category.count).to eq 2
+    expect(Feed.count).to eq 5
+  end
+
+  it "should create nothing if any user exists" do
+    create(:user)
+    load Rails.root.join("db", "seeds.rb")
+    expect(User.count).to eq 1
+    expect(Category.count).to eq 0
+    expect(Feed.count).to eq 0
+  end
+end # describe "seeds"
