5ca1e6517514fb17da9fecf0d1b71c2d8e903135
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2019-01-19 12:11:31 +0100

    Add config

--- config/application.rb
+++ config/application.rb
@@ -32,4 +32,16 @@ module Minifeed
 
     config.action_view.form_with_generates_remote_forms = false
   end
+
+  class Config
+    attr_accessor(
+      :autoimport_enabled,
+      :autoimport_interval,
+      :entries_per_page,
+    )
+  end
+
+  cattr_accessor :config
+
+  self.config = Config.new
 end
