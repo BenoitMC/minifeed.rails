2dc02f26ac5e20ee1f4610c12bf8d86c64955472
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2019-02-25 17:23:11 +0100

    Remove useless DJ initializer

--- config/initializers/delayed_job.rb
+++
@@ -1,9 +0,0 @@
-if defined? Delayed::Job
-  Rails.application.config.active_job.queue_adapter = :delayed_job
-
-  if Rails.env.test? || Rails.env.development?
-    Delayed::Worker.delay_jobs = false
-  else
-    Delayed::Worker.delay_jobs = true
-  end
-end
