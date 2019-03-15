c7693a1e0d1bc25d0c2b683fc2a71de64078de63
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2019-03-15 18:01:38 +0100

    Fix rufus-scheduler database errors

--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -6,6 +6,7 @@ end
 
 if Minifeed.config.autoimport_enabled
   scheduler.every "5s", first_in: "30s", overlap: false do
+    ApplicationRecord.connection.verify!
     Feed::ImportOutdatedService.call
   end
 end
