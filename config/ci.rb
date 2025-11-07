6158f26b2e5c75db9365547da5d89e4d10f1dae1
Benoit MARTIN-CHAVE <benoit@martin-chave.com>
2025-11-07 16:23:23 +0100

    Add missing tests on ci.rb

--- config/ci.rb
+++ config/ci.rb
@@ -3,6 +3,8 @@
 CI.run do
   step "Setup", "bin/setup --skip-server"
   step "Rubocop", "bin/rubocop -P"
+  step "Tests: Check file names", "bin/test-rspec-file-names"
   step "Tests: Rails", "bundle exec rspec --exclude-pattern 'system/*'"
   step "Tests: System", "bundle exec rspec spec/system"
+  step "Coverage", "bin/test-coverage"
 end
